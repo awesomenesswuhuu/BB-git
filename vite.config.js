95c3c90c3f456f04b55aceaa3ba4605d175a5ffc
awesomenesswuhuu <krathore@kent.edu>
2025-09-19 19:00:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: Set base to your repo's exact name, with leading & trailing slashes.
 export default defineConfig({
   plugins: [react()],
-  base: '/BB-git/', // works on GitHub Pages under /<repo>/ path
+  base: '/BB-git/', // e.g. '/beauty-kiosk-client-simple/'
 })
